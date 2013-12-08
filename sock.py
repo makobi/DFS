8fd0a6408a3d493666aa9b2725db0240686a0432
Makobi <a-leinad93@hotmail.com>
2013-12-08 18:27:03 -0400

    Added essay

--- sock.py
+++ sock.py
@@ -1,3 +1,5 @@
+""" Library by Adam Rosenfield http://stackoverflow.com/users/9530/adam-rosenfield """
+
 #Libraries
 import socket              # Library used for the socket functions in this program.
 import struct
@@ -9,19 +11,19 @@ def send_msg(sock, msg):
 
 def recv_msg(sock):
     # Read message length and unpack it into an integer
-    raw_msglen = recvall(sock, 4)
+    raw_msglen = recvall(sock, 4) # Receive data
     if not raw_msglen:
         return None
-    msglen = struct.unpack('>I', raw_msglen)[0]
+    msglen = struct.unpack('>I', raw_msglen)[0] # Unpack data
     # Read the message data
-    return recvall(sock, msglen)
+    return recvall(sock, msglen) # call function that uses len() if data is complete
 
 def recvall(sock, n):
     # Helper function to recv n bytes or return None if EOF is hit
     data = ''
-    while len(data) < n:
-        packet = sock.recv(n - len(data))
+    while len(data) < n: # if the size of the message is not equal to the total size
+        packet = sock.recv(n - len(data)) # Keep receiving 
         if not packet:
             return None
-        data += packet
-    return data
+        data += packet # accumulate message
+    return data # return message
